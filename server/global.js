0a027d71c9d62419c175055d17c4569b0a2c0f02
mandja96 <mandja96@gmail.com>
2020-07-02 13:45:13 +0200

    changed imageFolder path

--- server/global.js
+++ server/global.js
@@ -2,13 +2,15 @@ const express = require('express');
 const mysql = require('mysql');
 const redis = require('redis');
 const client  = redis.createClient(6379);
+const path = require('path')
 
 const db = mysql.createConnection({
 
     host: "127.0.0.1",
     user: "root",
     //password: "mysql25",
-	password: "Zimovrel9",
+    // password: "marsovac96"  // andja password
+	password: "marsovac96",
     database: "anime_central_db"
 /*
     host: "localhost",
@@ -20,9 +22,10 @@ const db = mysql.createConnection({
   });
 
 //const imageFolder = "C:\\Users\\Petar Zecevic\\Desktop\\12-AnimeCentral\\database\\anime_pictures\\"
-const imageFolder = "C:/Users/Lexxigar/Desktop/AnimePlanet/AnimeGit/12-animecentral/database/anime_pictures/"
+// const imageFolder = "C:/Users/Lexxigar/Desktop/AnimePlanet/AnimeGit/12-animecentral/database/anime_pictures/"
+const endIndex = __dirname.lastIndexOf('/');
+const imageFolder = path.join(__dirname.slice(0, endIndex), 'database/anime_pictures');
 
-  
 //function for hashing
 //Moved the hashedCode func to global in order to use it both in queries for unique ids and in user.
 //TODO: Consider moving it back in user after a solution is found for ids in queries.
