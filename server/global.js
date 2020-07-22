51cf118bd7f801942c96a40b3c5c1b1016a47895
PetarZecevic97 <petar.b.zecevic@gmail.com>
2020-07-22 19:13:32 +0200

    removed some comments

--- server/global.js
+++ server/global.js
@@ -8,8 +8,6 @@ const db = mysql.createConnection({
 
     host: "127.0.0.1",
 
-    //host: "localhost",
-
     user: "PetarZecevic",
     password: "mi16169",
 
@@ -25,18 +23,12 @@ const db = mysql.createConnection({
     //user : "root",
     //password: "Zimovrel9",  //coa password this is getting out of hand
 
-
-
     database: "anime_central_db"
   });
-
-//const imageFolder = "C:\\Users\\Petar Zecevic\\Desktop\\12-AnimeCentral\\database\\anime_pictures\\"
- //const imageFolder = "C:/Users/Lexxigar/Desktop/AnimePlanet/AnimeGit/12-animecentral/database/anime_pictures/"
 const endIndex = __dirname.lastIndexOf(path.sep);
 const imageFolder = path.join(path.join(__dirname.slice(0, endIndex), 'database'), 'anime_pictures');
+
 //function for hashing
-//Moved the hashedCode func to global in order to use it both in queries for unique ids and in user.
-//TODO: Consider moving it back in user after a solution is found for ids in queries.
 const hashCode = function(s){
   return s.split("").reduce((a,b) => {a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
 }
--- server/middleware/user_session.js
+++ server/middleware/user_session.js
@@ -80,46 +80,6 @@ const userSessions = {
             text: `You seem to have forgotten your password. Here's your recovery code: ${code}. It will expire in ten minutes so hurry up!`
         };
  
-        // sendmail(data, function(err, reply) {
-        //     console.log(err && err.stack);
-        //     console.dir(reply);
-        // });
-
-        // myCustomMethod = async function (ctx){
-        //     let cmd = await ctx.sendCommand(
-        //         'AUTH PLAIN ' +
-        //             Buffer.from(
-        //                 '\u0000' + ctx.auth.credentials.user + '\u0000' + ctx.auth.credentials.pass,
-        //                 'utf-8'
-        //             ).toString('base64')
-        //     );
-    
-        //     if(cmd.status < 200 || cmd.status >=300){
-        //         throw new Error('Failed to authenticate user: ' + cmd.text);
-        //     }
-        // }
-
-
-        // let transporter = nodemailer.createTransport({
-        //     // pool : true,
-        //     //  host: 'localhost',
-        //     //   port: 4200,
-        //       tls : { rejectUnauthorized: false },
-        //     // secure: true,
-        //      auth: {
-        //     //      type: 'custom',
-        //     //      method: 'MY-CUSTOM-METHOD', // forces Nodemailer to use your custom handler
-        //           user: 'username',
-        //           pass: 'verysecret'
-        //       }
-        //     // , customAuth: {
-        //     //      'MY-CUSTOM-METHOD': myCustomMethod
-        //     // }
-        //     // sendmail: true,
-        //     // newline: 'unix'
-        // });
-
-        // transporter.sendMail(data);
          mailgun.messages().send(data, function (error, body) {
              if (error) throw error;
            console.log(body);
